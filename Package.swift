2ad4708cc8e11ed01730297ff19b0fa477b41758
Schmidt Thibaut <thibaut@chargemap.com>
2020-11-13 14:03:31 +0100

    updated swift-tools

--- Package.swift
+++ Package.swift
@@ -24,6 +24,8 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
+// swift-tools-version:5.3
+
 import PackageDescription
 
 let package = Package(
